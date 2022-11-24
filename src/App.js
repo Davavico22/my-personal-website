7cae95e0e1d2147e569d0375b5ee61be0cda40a1
David Valdivia Vico <david19valdivia98@gmail.com>
2022-11-24 21:42:25 +0100

    Adaptadion a React Router Dom v6

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from "./Component/Home/Home";
 import Resume from "./Component/Resume/Resume";
 import PageNotFound from "./Component/PageNotFound/PageNotFound";
-import GA from "./GoogleAnalytics";
+// import GA from "./GoogleAnalytics";
 
 function App() {
   return (
     <BrowserRouter>
-    {GA.init()}
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/resume" component={Resume} />
-        <Route component={PageNotFound}/>
-      </Switch>
+    {/* {GA.init()} */}
+      <Routes>
+        <Route exact path='/' element = {<Home/>}/>
+        <Route exact path='/resume' element = {<Resume/>} />
+        <Route element = {<PageNotFound/>}/>
+      </Routes>
     </BrowserRouter>
   );
 }
