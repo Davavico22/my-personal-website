4202cf0e17ab1869f902cd484e8d02bf7f62d9db
David Valdivia <david19valdivia98@gmail.com>
2023-08-19 16:47:47 +0200

    Last commit update

--- src/Component/AppAds/AppAds.jsx
+++ src/Component/AppAds/AppAds.jsx
@@ -6,27 +6,24 @@ const AppAdsTxt = () => {
     const [content, setContent] = useState("");
 
     useEffect(() => {
-        var contenido='';
         axios
             .get(AppAds)
             .then((response) => {
                 setContent(response.data);
-                contenido=response.data;
+                const content = response.data; // Utilizamos el contenido del archivo importado
+                const blob = new Blob([content], { type: 'text/plain' });
+                const url = URL.createObjectURL(blob);
+        
+                const a = document.createElement('a');
+                a.href = url;
+                a.download = 'app-ads.txt';
+                a.click();
+        
+                URL.revokeObjectURL(url);
             })
             .catch((error) => {
                 console.error("Error loading app_ads.txt:", error);
             });
-
-        const content = contenido; // Utilizamos el contenido del archivo importado
-        const blob = new Blob([content], { type: 'text/plain' });
-        const url = URL.createObjectURL(blob);
-
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = 'app-ads.txt';
-        a.click();
-
-        URL.revokeObjectURL(url);
 
     }, []);
     return <pre>{content}</pre>;
